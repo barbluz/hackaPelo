7925fa779b0ef8f984dc4523a577cb5984cd9f22
Murphyly <isysnogueira@infojr.com.br>
2017-07-15 23:32:16 -0300

    Adiciona menu

---
+++ views/menu.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <?php 
+    include 'layouts/header.inc';
+    include 'layouts/head.inc';
+    ?>
+    <script type="text/js" src="../assets/js/style.js"></script>
+    <title>Menu</title>
+</head>
+<body>
+</body>
