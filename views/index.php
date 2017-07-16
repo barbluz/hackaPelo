b048765b20a52cdd1a45e61ac8d43372e1617bab
Gabriele Luz <gabrieleluz@users.noreply.github.com>
2017-07-15 22:54:15 -0300

    Inclui a navbar

---
+++ views/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php include '../includes/navigation.inc'; ?>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../assets/css/general.css">
+	<link rel="icon" type="image/png" href="">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title></title>
+</head>
+<body></body>
