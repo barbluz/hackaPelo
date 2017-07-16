2994639a5021888db819855c2778a59bf1333913
Murphyly <isysnogueira@infojr.com.br>
2017-07-16 00:21:02 -0300

    Adiciona mapa

---
+++ views/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+	    <?php 
+	    include 'layouts/header.inc';
+	    include 'layouts/head.inc';
+	    ?>
+	</head>
+	<body>
+		<div id="home">
+			<div id="descoberta">
+				<span>365 locais a descobrir</span>
+			<div id="oferta">
+				<span>Você tem 100,00</span>
+			</div>
+		</div>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.9417694283525!2d-38.51549905012711!3d-12.97557626335855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x71604eefb5cb14b%3A0x2af2e4cac016154b!2sCentro+Hist%C3%B3rico!5e0!3m2!1spt-BR!2sbr!4v1500174046909" width="" height="450" frameborder="0" style="border:0; width: 100%" allowfullscreen></iframe>
+	</body>
+</html>
+
